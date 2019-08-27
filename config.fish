a55efff4e182bbe6d93722316023c76bbb3e52ce
Sean Bethard <sean@seanbethard.com>
2019-08-27 10:40:26 -0500

    small catch

--- config.fish
+++ config.fish
@@ -99,7 +99,3 @@ if not functions -q fisher
     curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
     fish -c fisher
 end
-
-# fish gen
-
-{$HOME}/.rakudobrew/bin/rakudobrew init Fish | source
